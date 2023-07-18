a262b3a3abbd319d96815c45b91649b1080ca2a2
陈秋实 <chenqiushi@metrodata.cn>
2023-07-18 14:38:16 +0800

    目录修改

--- lib/perf-request.d.ts
+++
@@ -1,8 +0,0 @@
-export interface PerfRequestOptions {
-    cacheKey?: string;
-}
-export interface ObjectType {
-    [key: string]: any;
-}
-declare function perfRequest(service: () => Promise<any>, options?: PerfRequestOptions): Promise<any>;
-export default perfRequest;
